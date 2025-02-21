9cb20f41484ee6a74bcd89f7807ba94452eb57dd
William Olofsson Maronen <williamolofssonmaronen@gmail.com>
2025-02-21 21:55:40 +0100

    Add file descriptor

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-test
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+	int server_fd, new_socket;
+	struct sockaddr_in adress;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	int addrlen = sizeof(address);
+	char buffer[BUFFER_SIZE] = {0};
+
+	// Create a socket file descriptor
+	if ((server_fd = socket(AF_INET,SOCK_STREAM, 0))==0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+}
