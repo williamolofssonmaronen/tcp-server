30a399e7dc0dc707c8fba01e052238f5b34e5e3f
William Olofsson Maronen <williamolofssonmaronen@gmail.com>
2025-02-22 20:28:17 +0100

    Add CMake functionality

--- build/main.c
+++ build/main.c
@@ -7,17 +7,19 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main(){
+int main()
+{
 	int server_fd, new_socket;
 	struct sockaddr_in adress;
 	int opt = 1;
 	int addrlen = sizeof(adress);
-	int addrlen = sizeof(adress);
 	char buffer[BUFFER_SIZE] = {0};
 
 	// Create a socket file descriptor
-	if ((server_fd = socket(AF_INET,SOCK_STREAM, 0))==0) {
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
+	printf("Hello World!\n");
 }
