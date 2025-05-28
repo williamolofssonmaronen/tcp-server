ed6054eadf08595b135a6460a3f2c2f57d46c7da
William Olofsson Maronen <williamolofssonmaronen@gmail.com>
2025-05-28 22:14:57 +0200

    Final

--- matlab/wrapper.m
+++ matlab/wrapper.m
@@ -1,5 +1,5 @@
 %%
-flush(client);
+%flush(client); % May not run if not already initialized.
 clear all; close all;
 % SERVER DETAILS
 %serverIP = '192.168.50.125'; % Change to the server's IP address
@@ -117,3 +117,68 @@ xlabel('Time (us)')
 % write(client, "exit");
 % response = read(client, client.NumBytesAvailable, 'uint8');
 % disp(char(response));
+
+%%
+close all; clear;
+load("matlab.mat");
+delay_samples = 200;  % for example, delay by 500 samples
+rxSignal = rxSignal(:);
+rxSignal = [zeros(delay_samples, 1); rxSignal];
+
+[corr, lags] = xcorr(rxSignal, matched_preamble);
+[~, peak_idx] = max(abs(corr));
+frame_start = lags(peak_idx);
+preamble_len_samples = 63 * sps_rx;
+payload_start = frame_start + preamble_len_samples;
+aligned = rxSignal(payload_start+1:payload_start+numSymbols*sps_rx);
+aligned_downsampled = aligned(1:sps_rx:end);
+scatterplot(aligned_downsampled);
+
+% QAM Demodulation
+dataSymbolsOut = qamdemod(aligned_downsampled, M, 'gray', UnitAveragePower=true);
+% convert decimal values back to binary
+dataOutMatrix = de2bi(dataSymbolsOut, k, 'left-msb');
+% reshape binary matrix to a vector
+dataOut = dataOutMatrix(:);
+% calculate the number of bit errors
+numErrors = sum(data_bits ~= dataOut);
+% numErrors = sum(bitsIn ~= dataOut);
+disp(['Number of bit errors: ' num2str(numErrors)])
+disp(['Bit error rate: ' num2str(numErrors / numBits)])
+
+% Plot the correlation magnitude
+figure;
+plot(lags, abs(corr));
+hold on;
+xline(lags(peak_idx), 'r--', 'LineWidth', 2);
+title('Cross-correlation of recieved complex waveform with the matched preamble');
+xlabel('Lag');
+ylabel('Correlation Magnitude');
+legend('Cross-Correlation', 'Detected Frame Start');
+grid on;
+
+% Resample tx signal
+txSignal = resample(txSignal, sps_rx,sps_tx);
+% Normalize tx
+txSignal_norm = txSignal / max(abs(txSignal(preamble_len_samples+1:1:end)));
+% Normalize aligned
+aligned_norm = aligned / max(abs(aligned));
+% Plot total
+figure('Name','Total'), subplot(1,2,1)
+pwelch(txSignal,[],[],[],'centered',40e6)
+hold on
+pwelch(rxSignal,[],[],[],'centered',40e6)
+legend("Transmitted", "Recieved");
+subplot(1,2,2)
+plot((1:length(txSignal)+frame_start), [zeros(frame_start, 1); real(txSignal_norm)]);
+hold on
+plot((1:length(txSignal)+frame_start), [zeros(frame_start, 1); imag(txSignal_norm)]);
+plot((1:length(aligned)+payload_start), [zeros(payload_start, 1); real(aligned_norm)]);
+plot((1:length(aligned)+payload_start), [zeros(payload_start, 1); imag(aligned_norm)]);
+
+xline(payload_start, 'r--', 'LineWidth', 2);
+
+legend("Re(TX)", "Im(TX)", "Re(RX)", "Im(RX)", "Detected Payload Start");
+title("IQ Data")
+grid on
+xlabel('Time (us)')
